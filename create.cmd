deeaa1c3e0f80cc980a1ceb168c5d19a9f989612
Rohit Sharma <46834690+rohit133@users.noreply.github.com>
2022-12-04 22:43:44 +0530

    Update create.cmd

--- create.cmd
+++ create.cmd
@@ -8,7 +8,7 @@
     git add .
     git commit -m "Initial commit"
     git branch -M main
-    git remote add origin %c%%1.git
+    git remote add origin https://github.com/rohit133/%1.git
     git push -u origin main
     code .
 EXIT /B 0   
